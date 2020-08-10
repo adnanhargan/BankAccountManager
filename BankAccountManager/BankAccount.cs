d5a216ed6511c5cc2dfb75e83ac9a5d3826f9c52
adnanhargan <adnanhargan101@gmail.com>
2020-08-10 08:00:25 +0500

    Add project files.

---
+++ BankAccountManager/BankAccount.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks.Dataflow;
+
+namespace BankAccountManager
+{
+    class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach( var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        private static int accountNumberSeed = 1234567890;
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+        }
+        public void MakeDeposit(Decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of the deposit must be positive");
+            }
+
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "amount of the withdrawal must be positive");
+            }
+            if(Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for the withdrawal");
+            }
+
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            report.AppendLine("Date\t\t Amount \t Notes");
+            foreach (var item in allTransactions)
+            {
+                report.AppendLine($" {item.Date.ToShortDateString()} \t {item.Amount}\t\t {item.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ BankAccountManager/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankAccountManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account1 = new BankAccount("Adnan", 10000);
+            Console.WriteLine($"{account1.Number} was created for {account1.Owner} with {account1.Balance} initial balance ");
+
+            Console.WriteLine(account1.GetAccountHistory());
+
+        }
+    }
+}
